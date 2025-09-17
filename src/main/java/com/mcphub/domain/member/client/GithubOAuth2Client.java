f7faf4078833ea14d228bba2a31f8dfaf719a902
asdfjk123 <minjae20000514@gmail.com>
2025-09-17 14:42:01 +0900

    [feat] add github social login

--- src/main/java/com/mcphub/domain/member/client/GithubOAuth2Client.java
+++ src/main/java/com/mcphub/domain/member/client/GithubOAuth2Client.java
@@ -1,9 +1,15 @@
 package com.mcphub.domain.member.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.mcphub.domain.member.client.properties.GithubOAuth2Properties;
+import com.mcphub.domain.member.dto.response.readmodel.GithubProfile;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Collections;
 
 @Component
 @RequiredArgsConstructor
@@ -12,5 +18,29 @@ public class GithubOAuth2Client {
     private final WebClient webClient = WebClient.create();
     private final GithubOAuth2Properties githubOAuth2Properties;
 
+    public GithubProfile getProfile(String code) {
+        String accessToken = webClient.post()
+                .uri("https://github.com/login/oauth/access_token")
+                .headers(headers -> {
+                    headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));  // JSON 응답 받기
+                    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);             // 요청 body 타입
+                })
+                .body(BodyInserters.fromFormData("client_id", githubOAuth2Properties.getClientId())
+                        .with("client_secret", githubOAuth2Properties.getClientSecret())
+                        .with("redirect_uri", githubOAuth2Properties.getRedirectUri())
+                        .with("code", code))
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .block()
+                .get("access_token").asText();
 
+        return webClient.get()
+                .uri("https://api.github.com/user")
+                .header("Authorization", "token " + accessToken)
+                .headers(headers -> headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON)))
+                .retrieve()
+                .bodyToMono(GithubProfile.class)
+                .block();
+
+    }
 }
---
+++ src/main/java/com/mcphub/domain/member/dto/response/readmodel/GithubProfile.java
@@ -0,0 +1,12 @@
+package com.mcphub.domain.member.dto.response.readmodel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GithubProfile {
+
+    private String id;
+    private String login;
+}

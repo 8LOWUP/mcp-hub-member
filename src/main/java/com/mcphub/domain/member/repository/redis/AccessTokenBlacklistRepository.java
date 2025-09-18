c91f901d7d538a41411776c25f26fcd4d06caa4f
asdfjk123 <minjae20000514@gmail.com>
2025-09-18 23:37:49 +0900

    [refactor] move jwt token entity to global directory

--- src/main/java/com/mcphub/domain/member/repository/redis/AccessTokenBlacklistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mcphub.domain.member.repository.redis;
-
-import com.mcphub.domain.member.entity.AccessTokenBlacklist;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccessTokenBlacklistRepository extends CrudRepository<AccessTokenBlacklist, String> {
-}
